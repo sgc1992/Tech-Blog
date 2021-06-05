e2f65468f251721dde3f2340666c2e7eddee2b93
Sandeep GC <sandeep_gc@hotmail.com>
2021-06-05 15:47:34 +1000

    api routes for comment

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
