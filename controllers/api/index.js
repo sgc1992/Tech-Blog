58cf0569b3aa3333bc4a43a12c1d1563b1881c58
Sandeep GC <sandeep_gc@hotmail.com>
2021-06-05 15:59:47 +1000

    added routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const projectRoutes = require('./post-routes');
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
-
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
 module.exports = router;
