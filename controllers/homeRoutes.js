d0b38b489045906597ea223646358bd70175cb68
Sandeep GC <sandeep_gc@hotmail.com>
2021-06-10 20:25:23 +1000

    added comment is the homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { blog, User } = require('../models');
+const { blog,comment, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
