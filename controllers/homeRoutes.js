8f6f20179b5a78ec07de2fc0b8c8da0628044732
Sandeep GC <sandeep_gc@hotmail.com>
2021-06-10 16:02:57 +1000

    added maxage to cookies

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -45,7 +45,7 @@ router.get('/blog/:id', async (req, res) => {
       logged_in: req.session.logged_in
     });
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err.message);
   }
 });
 
